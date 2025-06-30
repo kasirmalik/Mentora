e1ee1e2e722cbdbf0982065a9079033cdd958056
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-06-30 10:28:23 +0530

    Add educator and student components for improved UI structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import CoursesList from './pages/student/CoursesList';
 import CourseDetails from './pages/student/CourseDetails';
 import MyEnrollments from './pages/student/MyEnrollments';
 import Player from './pages/student/Player';
-import Loading from './students/Loading';
+import Loading from './components/students/Loading';
 import Educator from './pages/educator/Educator';
 import Dashboard from './pages/educator/Dashboard';
 import AddCourse from './pages/educator/AddCourse';
