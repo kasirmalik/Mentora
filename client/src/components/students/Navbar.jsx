acc73382696542b7f61e9cd30c536690c4aafa36
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-06-30 13:02:53 +0530

    Integrate Clerk authentication by adding ClerkProvider and updating Navbar for user sign-in functionality

--- client/src/components/students/Navbar.jsx
+++ client/src/components/students/Navbar.jsx
@@ -1,21 +1,36 @@
 import React from 'react'
 import { assets } from '../../assets/assets.js';
 import { Link } from 'react-router-dom';
+import { useClerk,UserButton,useUser } from '@clerk/clerk-react';
 
 function Navbar() {
   const isCourseListPage = location.pathname.includes('/course-list');
 
+  const {openSignIn} = useClerk();
+  const { user } = useUser();
+
+
   return (
+    // for large screens
     <div className={`flex justify-between items-center px-4  py-4  sm:px-10  md:px-14 lg:px-36 border-b border-gray-500  ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}` }>
      <img src={assets.logo} alt="Logo"  className='w-28 lg:w-32 cursor-pointer'/>
      <div className='hidden md:flex items-center gap-5 text-gray-500'>
-        <div>
+        <div className='flex items-center gap-5' >
           <button>Become Educator</button>
           | <Link to={'/my-enrollments'}>My Enrollments</Link>
         </div>
-        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+       {user ? <UserButton/>: 
+        <button onClick={()=>openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+       }
      </div>
-     <div></div>
+     {/* for phone screen */}
+     <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+          <div>
+             <button>Become Educator</button>
+          | <Link to={'/my-enrollments'}>My Enrollments</Link>
+          </div>
+          <button><img src={assets.user_icon} alt="user-icon" /></button>
+     </div>
     </div>
   )
 }
