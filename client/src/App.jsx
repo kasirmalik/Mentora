9b6b9b066aae684052f51daf002d32ce69be832a
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-06-30 10:25:05 +0530

    Add educator routes and components for course management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,11 @@ import CourseDetails from './pages/student/CourseDetails';
 import MyEnrollments from './pages/student/MyEnrollments';
 import Player from './pages/student/Player';
 import Loading from './students/Loading';
+import Educator from './pages/educator/Educator';
+import Dashboard from './pages/educator/Dashboard';
+import AddCourse from './pages/educator/AddCourse';
+import MyCourses from './pages/educator/MyCourses';
+import StudentsEnrolled from './pages/educator/StudentsEnrolled';
 
 function App() {
   return (
@@ -23,7 +28,12 @@ function App() {
       <Route path='/loading/:path' element={<Loading/>}/>
 
       {/* route for educator */}
-      
+      <Route path='/educator' element={<Educator/>}>
+        <Route path='educator' element={<Dashboard/>}/>
+        <Route path='add-course' element={<AddCourse/>}/>
+        <Route path='my-courses' element={<MyCourses/>}/>
+        <Route path='students-enrolled' element={<StudentsEnrolled/>}/>
+      </Route>
 
      </Routes>
     </div>
--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 function Educator() {
   return (
     <div>
       Educator
+      <div className='container'>
+        {<Outlet/>}
+        </div>
     </div>
   )
 }
