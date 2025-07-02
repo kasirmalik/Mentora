b5064d8d9d548f65e58fc044373d4dadf38ac4d1
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-07-02 10:13:35 +0530

    Refactor CourseCard and CoursesSection components to utilize AppContext for course data; enhance CourseCard with detailed course information and styling

--- client/src/components/students/CoursesSection.jsx
+++ client/src/components/students/CoursesSection.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
+import CourseCard from './CourseCard'
 
 function CoursesSection() {
+  const {allCourses} = useContext(AppContext)
   return (
     <div className='py-16 md:px40 px-8'>
       <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
       <p className='text-sm md:text-base text-gray-500 mt-3'>Explore a wide range of courses, from programming to design, and take the first step towards your dream career.</p>
 
+      <div>
+        {allCourses.slice(0,4).map((course,index)=> <CourseCard key={index} course={course}/>)}
+      </div>
       <Link to={'/course-list'} onClick={()=> scrollTo(0,0)}
       className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'
       >Show All Courses</Link>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,12 +1,25 @@
 
-import { createContext } from 'react'
+import { createContext, useEffect, useState } from 'react'
+import { dummyCourses } from '../assets/assets';
 
 export const AppContext = createContext();
 
 
 export const AppContextProvider = (props)=>{
-    const value = {
 
+    const currency = import.meta.env.VITE_CURRENCY
+    const [allCourses,setAllCourses]=useState([])
+
+    // fetch all courses
+    const fetchAllCourses = async ()=>{
+        setAllCourses(dummyCourses)
+    }
+    useEffect(() => {
+       fetchAllCourses()
+    }, []);
+    const value = {
+       currency,
+       allCourses
     }
     return (
         <AppContext.Provider value={value}>
