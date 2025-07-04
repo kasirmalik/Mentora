8926ab919af36836e3f750ae72be16ea01ea159a
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-07-04 09:50:58 +0530

    Implement CourseDetails component to fetch and display course information; add Loading component for data fetching state.

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { AppContext } from '../../context/AppContext';
+import Loading from '../../components/students/Loading';
 
 function CourseDetails() {
-  return (
-    <div>
-      
+  const {id} = useParams();
+
+  // Fetch course details using the id
+  const [courseData, setCourseData] = useState(null);
+  const {allcourses} = useContext(AppContext);
+
+  const fetchCourseDeta = async() => {
+    const findCourse =  allcourses.find(course => course._id === id) 
+    setCourseData(findCourse);
+  }
+  useEffect(() => {
+    fetchCourseDeta();
+  }, []);
+
+  return courseData ? (
+    <>
+    <div className='flex md:flex-row  md:gap-0 items-start justify-between
+    md:pt-30  md:px-36 px-8 pt-20 text-left gap-10 relative flex-col-reverse'>
+
+      <div className="absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from bg-cyan-100/70"></div>
+      {/* left colum */}
+      <div>
+        <h1>{courseData.courseTitle}</h1>
+        <p>{courseData.courseDescription}</p>
+      </div>
+      {/* Right colum */}
     </div>
-  )
+    </>
+  ):<Loading/>
 }
 
 export default CourseDetails
