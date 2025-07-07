2192222ac9ce9953249880d368b0aa0a2bd89dca
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-07-07 09:13:24 +0530

    Add loading spinner and video player functionality in CourseDetails component; integrate Footer component

--- client/src/components/students/Loading.jsx
+++ client/src/components/students/Loading.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Loading() {
   return (
-    <div>
-      Loading...
+    <div className='flex items-center justify-center min-h-screen'>
+      <div className="w-16 sm:w-20 aspect-square border-4 border-gray-300 border-t-4 border-t-blue-400 rounded-full animate-spin"></div>
     </div>
   )
 }
--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -4,6 +4,8 @@ import { AppContext } from "../../context/AppContext";
 import Loading from "../../components/students/Loading";
 import { assets } from "../../assets/assets";
 import humanizeDuration from "humanize-duration";
+import Footer from '../../components/students/Footer'
+import Youtube from 'react-youtube'
 
 function CourseDetails() {
   const { id } = useParams();
@@ -11,6 +13,8 @@ function CourseDetails() {
   // Fetch course details using the id
   const [courseData, setCourseData] = useState(null);
   const [openSection, setOpenSection] = useState({});
+  const [isAlreadyEnrolled, setisAlreadyEnrolled] = useState(false);
+  const [playerData, setplayerData] = useState(null);
   const {
     allCourses,
     calculateRating,
@@ -26,7 +30,7 @@ function CourseDetails() {
   };
   useEffect(() => {
     fetchCourseDeta();
-  }, []);
+  }, [allCourses]);
 
   // Function to toggle section open/close
   const toggleSection = (index) => {
@@ -129,7 +133,11 @@ function CourseDetails() {
                             <p>{lecture.lectureTitle}</p>
                             <div className="flex gap-2">
                               {lecture.isPreviewFree && (
-                                <p className="text-blue-500 cursor-pointer">
+                                <p 
+                                onClick={()=> setplayerData({
+                                  videoId:lecture.lectureUrl.split('/').pop()
+                                })}
+                                 className="text-blue-500 cursor-pointer">
                                   Preveiw
                                 </p>
                               )}
@@ -163,16 +171,37 @@ function CourseDetails() {
         </div>
         {/* Right colum */}
         <div className="max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:w-[420px] ">
-          <img src={courseData.courseThumbnail} alt="" />
+           {
+                  playerData ? <Youtube videoId={playerData.videoId} opts={{playerVars:{autoplay:1}}} iframeClassName="w-full aspect-vedio"/>
+
+                  : <img src={courseData.courseThumbnail} alt="" />
+                }
+
+          
           <div className="p-5">
             <div className="flex items-center gap-2">
+               
               <img className="w-3.5" src={assets.time_left_clock_icon} alt="" />
-              <p className="text-red-500"> <span className="font-medium">5 days</span>left at this price!</p>
+              <p className="text-red-500">
+                {" "}
+                <span className="font-medium">5 days</span>left at this price!
+              </p>
             </div>
             <div className="flex items-center gap-3 pt-2">
-              <p className="text-gray-800 md:text-4xl text-2xl font-semibold">{currency}{(courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toFixed(2)}</p>
-              <p className="md:text-lg to-gray-500 line-through">{currency}{courseData.coursePrice}</p>
-              <p className="md:text-lg to-gray-500">{courseData.discount}% off</p>
+              <p className="text-gray-800 md:text-4xl text-2xl font-semibold">
+                {currency}
+                {(
+                  courseData.coursePrice -
+                  (courseData.discount * courseData.coursePrice) / 100
+                ).toFixed(2)}
+              </p>
+              <p className="md:text-lg to-gray-500 line-through">
+                {currency}
+                {courseData.coursePrice}
+              </p>
+              <p className="md:text-lg to-gray-500">
+                {courseData.discount}% off
+              </p>
             </div>
 
             <div className="flex items-center text-sm md:text-default gap-4 pt-2 md:pt-4 text-gray-500">
@@ -183,22 +212,34 @@ function CourseDetails() {
 
               <div className="h-4 wpx bg-gray-500/40"></div>
               {/* display */}
-               <div className="flex items-center gap-1">
+              <div className="flex items-center gap-1">
                 <img src={assets.time_clock_icon} alt="" />
                 <p>{calculateCourseDuration(courseData)}</p>
               </div>
               <div className="h-4 wpx bg-gray-500/40"></div>
               {/* lectures */}
-                 <div className="flex items-center gap-1">
+              <div className="flex items-center gap-1">
                 <img src={assets.lesson_icon} alt="" />
                 <p>{calculateNumberOfLectures(courseData)} lessons</p>
               </div>
-
-              
+            </div>
+            <button className="md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium">
+              {isAlreadyEnrolled ? "Already Enrolled" : "Enroll Now"}
+            </button>
+            <div className="pt-6">
+              <p className="md:text-xl text-lg font-medium text-gray-600">What`s in the course?</p>
+              <ul className="ml-4 pt-2 text-sm md:text-default list-disc text-gray-500">
+                <li>Lifetime acess with free updates</li>
+                <li>Step by step, hands-on project guidance</li>
+                <li>Downloadable resources and source code</li>
+                <li>Quizzesbto tes your knowledge</li>
+                <li>Certificate of completion</li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
+      <Footer/>
     </>
   ) : (
     <Loading />
