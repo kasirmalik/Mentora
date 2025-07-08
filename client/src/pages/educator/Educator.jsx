10ccb9769300a209896d64895e13fc7492b4c0e7
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-07-08 22:27:48 +0530

    Refactor Navbar and Educator components; enhance layout and user greeting functionality

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../../components/educator/Navbar'
 
 function Educator() {
   return (
-    <div>
-      Educator
+    <div className='text-default min-h-screen bg-white'>
+      <Navbar/>
       <div className='container'>
         {<Outlet/>}
         </div>
