bb83055c0ade211a109cf489d0366aa5e8af78e1
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-07-09 11:26:12 +0530

    Refactor Educator component structure; improve layout and organization

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
+    <footer className='flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t'>
+      <div className='flex items-center gap-4'>
+        <img className='hidden md:block w-20' src={assets.logo} alt="" />
+        <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
+        <p className='py-4 text-center text-xs md:text-sm text-gray-500'>
+         Copyright 2025 &copy;  Knowledge Hub. All rights reserved.
+        </p>
+      </div>
+      <div className='flex gap-4'>
+        <a href="#">
+          <img src={assets.facebook_icon} alt="" />
+        </a>
+        <a href="#">
+          <img src={assets.twitter_icon} alt="" />
+        </a>
+        <a href="#">
+          <img src={assets.instagram_icon} alt="" />
+        </a>
+      </div>
+    </footer>
   )
 }
 
