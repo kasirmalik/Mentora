ef5c38968407931aeffa6f4d4f3439197073d2cf
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-07-10 20:10:34 +0530

    Refactor AddCourse component; implement state management and integrate Quill editor for course description

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import uniqid from 'uniqid'
+import Quill from 'quill'
 
 function AddCourse() {
+  const quillRef = useRef(null);
+  const editorRef = useRef(null);
+
+const [courseTitle,setCourseTitle] = useState('')
+const [coursePrice,setCoursePrice] = useState(0)
+const [discount,setDiscount] = useState(0)
+const [image,setImage] = useState(null)
+const [chapters,setChapter] = useState([])
+const [showPopup,setShowPopup] = useState(false)
+const [currentChapterId,setCurrentChapterId] = useState(null)
+const [lectureDetils,setLectureDetails] = useState(
+  {
+    lectureTitle:'',
+    lectureDuration:'',
+    lectureUrl:'',
+    isPreviewFree:false
+  }
+)
+
+
+useEffect(()=>{
+  if (!quillRef.current && editorRef.current) {
+    quillRef.current = new Quill(editorRef.current,{
+      theme:'snow',
+    });
+  }
+},[])
+
+
   return (
-    <div>
-      Add Course
+    <div className='h-screen overflow-scroll flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0'>
+      <form action="">
+        <div className='flex flex-col gap-1'>
+          <p>Course Title</p>
+          <input type="text"
+          onChange={e => setCourseTitle(e.target.value)}
+          placeholder='Type here'
+          className='outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500'
+          required
+          value={courseTitle}
+           />
+        </div>
+        <div className="flex flex-col gap-1">
+          <p>Course Description</p>
+          <div ref={editorRef}></div>
+        </div>
+      </form>
     </div>
   )
 }
