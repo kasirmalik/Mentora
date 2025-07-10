b9eb87faf172265526b0f71d86a65ef89272a14a
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-07-10 10:04:44 +0530

    Refactor MyCourses component; enhance layout and improve table structure

--- client/src/pages/educator/MyCourses.jsx
+++ client/src/pages/educator/MyCourses.jsx
@@ -17,8 +17,8 @@ function MyCourses() {
     <div className='h-screen flex flex-col items-start justify-between md:p-8 md:pb-0 pt-8 pb-0'>
      <div className='w-full'>
       <h2 className="pb-4 text-lg font-medium">My Courses</h2>
-        <div>
-          <table>
+        <div className='flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-500/20'>
+          <table className='md:table-auto table-fixed w-full overflow-hidden'>
             <thead className='text-gray-900 border-b border-gray-500/20 text-sm text-left'>
                 <tr>
                   <th className="px-4 py-3 font-semibold truncate">All Courses</th>
@@ -27,6 +27,23 @@ function MyCourses() {
                   <th className="px-4 py-3 font-semibold truncate">Published On</th>
                 </tr>
             </thead>
+            <tbody className='text-sm text-gray-500'>
+               {courses.map((course)=>(
+                <tr key={course._id} className='border-b bordergr500/20'>
+                  <td className='md:px-4 pl-2 md:pl-4 py-3 flex items-center space-x-3 truncate'>
+                    <img src={course.courseThumbnail} alt=""  className='w-16'/>
+                    <span className='truncate hidden md:block'>{course.courseTitle}</span>
+                  </td>
+                  <td className='px-4 py-3'>
+                    {currency} {Math.floor(course.enrolledStudents.length * (course.coursePrice - course.discount * course.coursePrice / 100))}
+                  </td>
+                  <td className='px-4 py-3'>{course.enrolledStudents.length}</td>
+                  <td className='px-4 py-3'>
+                    {new Date(course.createdAt).toLocaleDateString()}
+                  </td>
+                </tr>
+               ))}
+            </tbody>
           </table>
         </div>
      </div>
