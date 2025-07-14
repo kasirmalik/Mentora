26acfa1d2773cad5c06f877686218cbe5bb9c542
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-07-14 09:46:17 +0530

    feat(server): add POST route for Clerk webhooks handling

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
+import { clerkWebhooks } from './controllers/webhooks.js'
 
 //Intialize Express
 const app = express()
@@ -17,7 +18,7 @@ app.use(cors())
 app.get('/',(req,res)=>{
     res.send("api working")
 })
-
+app.post('/clerk',express.json(),clerkWebhooks)
 //Port 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,()=>{
