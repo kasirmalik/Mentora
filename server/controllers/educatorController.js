812e2beb2bbf8c5bddfc6c8bdf46895b00549382
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-07-15 10:24:24 +0530

    Added new Route educator role

---
+++ server/controllers/educatorController.js
@@ -0,0 +1,16 @@
+import {clerkClient} from '@clerk/express'
+
+export const updateRoleToEducator = async (req,res)=>{
+    try {
+        const userId = req.auth.userId
+        await clerkClient.users.updateUserMetadata(userId,{
+            publicMetadata:{
+                role: 'educator',
+            }
+        })
+         res.json({success:true,message:'You can Publish Course Now'});
+
+    } catch (error) {
+         res.json({success:false,message:error.message});
+    }
+}
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educatorRoute.js'
+import { clerkMiddleware } from '@clerk/express'
 
 //Intialize Express
 const app = express()
@@ -13,12 +15,14 @@ await connectDB()
 // Middleware
 
 app.use(cors())
+app.use(clerkMiddleware)
 
 //Routes
 app.get('/',(req,res)=>{
     res.send("api working")
 })
 app.post('/clerk',express.json(),clerkWebhooks)
+app.use('/api/educator',express.json(educatorRouter))
 //Port 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,()=>{
